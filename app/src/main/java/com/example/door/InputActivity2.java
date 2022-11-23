ad24a247bdce03aaf4cb7ba076f52f56fb26ad62
alexaLL2000 <113215257+alexaLL2000@users.noreply.github.com>
2022-11-22 23:57:02 -0500

    Add files via upload

--- app/src/main/java/com/example/door/InputActivity2.java
+++ app/src/main/java/com/example/door/InputActivity2.java
@@ -2,14 +2,20 @@ package com.example.door;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 
 public class InputActivity2 extends AppCompatActivity implements View.OnClickListener{
 
     RelativeLayout nextLyt;
+    EditText numEdt;
+    String st;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,14 +27,23 @@ public class InputActivity2 extends AppCompatActivity implements View.OnClickLis
 
         nextLyt = findViewById(R.id.nextLyt);
         nextLyt.setOnClickListener(this);
+
+        numEdt = findViewById(R.id.numEdt);
+
+       SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences(getString(R.string.Door_SharedPreferences), Context.MODE_PRIVATE);
+       String door = sharedPreferences.getString(getString(R.string.Door_SharedPreferences_Name_Key),null);
+
+
     }
 
     @Override
     public void onClick(View v) {
 
         if (v == nextLyt){
-
-            startActivity(new Intent(InputActivity2.this, Home.class));
+            Intent intent = new Intent(InputActivity2.this, Home.class);
+            st = numEdt.getText().toString();
+            intent.putExtra("Value",st);
+            startActivity(intent);
             finish();
         }
     }
