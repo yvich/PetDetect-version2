dc04511969c3b0452e4d72a89726e3cf65d9a0d9
alexaLL2000 <113215257+alexaLL2000@users.noreply.github.com>
2022-11-23 22:13:40 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/door/MainActivity.java
+++ app/src/main/java/com/example/door/MainActivity.java
@@ -1,18 +1,32 @@
 package com.example.door;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
 
     TextView signupTxt;
     RelativeLayout signinLyt;
+    TextView forgotPasswordTxt;
+
+    protected EditText emailEdt;
+    protected EditText passEdt;
+
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,8 +39,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         signupTxt = findViewById(R.id.signupTxt);
         signupTxt.setOnClickListener(this);
 
+
+        forgotPasswordTxt = findViewById(R.id.forgotPasswordTxt);
+        forgotPasswordTxt.setOnClickListener(this);
+
         signinLyt = findViewById(R.id.signinLyt);
         signinLyt.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailEdt = findViewById(R.id.emailEdt);
+        passEdt = findViewById(R.id.passEdt);
     }
 
     @Override
@@ -39,9 +62,44 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
 
         if (v == signinLyt){
+            loginUserAccount();
+        }
 
-            startActivity(new Intent(MainActivity.this, Home.class));
-            finish();
+        if (v == forgotPasswordTxt){
+            Intent intent = new Intent(MainActivity.this, forgotPass.class);
+            startActivity(intent);
         }
     }
+
+    private void loginUserAccount() {
+        String email, password;
+        email = emailEdt.getText().toString();
+        password = passEdt.getText().toString();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Please enter email", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Please enter password", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(getApplicationContext(), "Login successful!", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(MainActivity.this, Home.class);
+                    startActivity(intent);
+                }
+
+                else {
+                    Toast.makeText(getApplicationContext(), "Login failed! Please try again", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+
 }
